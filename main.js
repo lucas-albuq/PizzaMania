47380deb286b6158ca02f49a680bd8dc6105dc10
Lucas Francisco de Albuquerque Barbosa <20221011110001@ifrn.local>
2024-01-05 08:36:11 -0300

    Página de contato

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const header = document.getElementById('header');
     const footer = document.getElementById('footer');
+    let tituloElements = document.querySelectorAll(".titulo")
 
     fetch('/assets/html/header.html')
         .then(response => response.text())
@@ -15,5 +16,18 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.text())
         .then(data => {
             footer.innerHTML = data;
+        });
+    tituloElements.forEach((titulo) => {
+        titulo.addEventListener("click", function (){
+            let questionDiv = titulo.parentNode;
+            
+            if (questionDiv.classList.contains("not-visible")) {
+            questionDiv.classList.remove("not-visible");
+            questionDiv.classList.add("visible");
+            } else {
+            questionDiv.classList.remove("visible");
+            questionDiv.classList.add("not-visible");
+            }
+        });
         });
 });
